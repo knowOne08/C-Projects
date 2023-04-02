a8a42276d3fe3ac4feceac8585e9339c9a0d5de3
Yash Darji <117254962+knowOne08@users.noreply.github.com>
2023-04-02 23:04:39 +0530

    Streak

--- SchlWork/examPrac/pyramid2.c
+++ SchlWork/examPrac/pyramid2.c
@@ -1,16 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+ 
 
-int main(){
-    int i,j,k=1;
+int main()
+{
 
-    for(i=1;i<=3;i++){
+    int i, space, n = 5, j = 0;
+ 
 
-        for(j=1;j<=i;j++){
-            printf("%d ",k);
-            k++;
+    // first for loop is used to
+
+    // iterate number of rows
+
+    for (i = 0; i < n - 1; i++) {
+ 
+
+        // second for loop is used to print spaces
+
+        for (space = 1; space < n - i; space++) {
+
+            printf(" ");
+
         }
+
+        // third for loop is used
+
+        // to print the required
+
+        // pattern
+
+        for (j = 0; j <= 2 * i; j++) {
+
+            if (j == 0 || j == 2 * i)
+
+                printf("*");
+
+            else
+
+                printf(" ");
+
+        }
+
+        // print the new line after every row
+
         printf("\n");
+
+    }
+
+    // used to print last row
+
+    for (i = 0; i < 2 * n - 1; i++) {
+
+        printf("*");
 
     }
 

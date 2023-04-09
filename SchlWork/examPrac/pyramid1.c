70a9a08bca53f84c1acbd2b3e71981f9858badf9
Yash Darji <117254962+knowOne08@users.noreply.github.com>
2023-04-09 21:54:59 +0530

    Update pyramid1.c

--- SchlWork/examPrac/pyramid1.c
+++ SchlWork/examPrac/pyramid1.c
@@ -1,39 +1,17 @@
-include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    int rows;
-    int column;
-    int spaces;
-    printf("Enter the number of rows : ");
-    scanf(" %d",&rows);
-    //diamond pattern
-    printf("****--diamond pattern---****\n");
-    for(int i=0; i<rows; i++)
-    {
-        for(int l=0;l<rows-1-i;l++)
-        {
-            printf(" ");
-        }
-        for(int j=0; j<=i;j++)
-        {
-            printf("%c ",'*');
-        }
-        printf("\n");
-    }
-    for(int i=0; i<rows; i++)
-    {
-        for(int spaces=0;spaces<i;spaces++)
-        {
-            printf(" ");
-        }
-        for(int j=0;j<rows-i;j++)
-        {
-            printf("%c ",'*');
-        }
-        printf("\n");
-    }
- 
-    printf("\n");
-    return 0;
+#include <stdio.h>
+int main() {
+   int i, space, rows, k = 0;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i, k = 0) {
+      for (space = 1; space <= rows - i; ++space) {
+         printf("  ");
+      }
+      while (k != 2 * i - 1) {
+         printf("* ");
+         ++k;
+      }
+      printf("\n");
+   }
+   return 0;
 }

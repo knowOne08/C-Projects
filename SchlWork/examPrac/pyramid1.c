0a5a434bc3ed1af14b8b607ef646cef870e5c79f
Yash Darji <117254962+knowOne08@users.noreply.github.com>
2023-04-14 23:02:03 +0530

    Update pyramid1.c

--- SchlWork/examPrac/pyramid1.c
+++ SchlWork/examPrac/pyramid1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 int main() {
-   int i, j, rows;
+   int rows, i, j, space;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
    for (i = rows; i >= 1; --i) {
-      for (j = 1; j <= i; ++j) {
+      for (space = 0; space < rows - i; ++space)
+         printf("  ");
+      for (j = i; j <= 2 * i - 1; ++j)
          printf("* ");
-      }
+      for (j = 0; j < i - 1; ++j)
+         printf("* ");
       printf("\n");
    }
    return 0;

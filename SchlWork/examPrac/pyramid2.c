4b35854588266c234972fb78dcfb22e91fc6710c
Yash Darji <117254962+knowOne08@users.noreply.github.com>
2023-04-17 22:28:56 +0530

    Update pyramid2.c

--- SchlWork/examPrac/pyramid2.c
+++ SchlWork/examPrac/pyramid2.c
@@ -1,25 +1,19 @@
-#include <stdio.h>  
-#include <conio.h>  
-void main()  
-{  
-    int num, i, j, m = 1; // declare local variables  
-    printf (" Enter the number to define the columns: \n");  
-    scanf ("%d", & num);  
-    for (i = 1; i <= num; i++)  
-    {  
-        for (j = 1; j <= i; j++)  
-        {  
-            printf( "* ");  
-        }  
-        printf("\n");  
-    }  
-    for (i = num-1; i >= 1; i--)  
-    {  
-        for (j = 1; j <= i; j++)  
-        {  
-            printf ("* ");  
-        }  
-        printf("\n");  
-    }  
-    getch();  
-}  
+#include <stdio.h>
+int main() {
+   int rows, coef = 1, space, i, j;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 0; i < rows; i++) {
+      for (space = 1; space <= rows - i; space++)
+         printf("  ");
+      for (j = 0; j <= i; j++) {
+         if (j == 0 || i == 0)
+            coef = 1;
+         else
+            coef = coef * (i - j + 1) / j;
+         printf("%4d", coef);
+      }
+      printf("\n");
+   }
+   return 0;
+}

f23c59c1330b2036067bea4ab9232d5b1a83572f
Yash Darji <117254962+knowOne08@users.noreply.github.com>
2023-04-19 23:01:40 +0530

    Update pyramid2.c

--- SchlWork/examPrac/pyramid2.c
+++ SchlWork/examPrac/pyramid2.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 int main() {
-   int rows, coef = 1, space, i, j;
+   int i, space, rows, k = 0, count = 0, count1 = 0;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
-   for (i = 0; i < rows; i++) {
-      for (space = 1; space <= rows - i; space++)
+   for (i = 1; i <= rows; ++i) {
+      for (space = 1; space <= rows - i; ++space) {
          printf("  ");
-      for (j = 0; j <= i; j++) {
-         if (j == 0 || i == 0)
-            coef = 1;
-         else
-            coef = coef * (i - j + 1) / j;
-         printf("%4d", coef);
+         ++count;
       }
+      while (k != 2 * i - 1) {
+         if (count <= rows - 1) {
+            printf("%d ", i + k);
+            ++count;
+         } else {
+            ++count1;
+            printf("%d ", (i + k - 2 * count1));
+         }
+         ++k;
+      }
+      count1 = count = k = 0;
       printf("\n");
    }
    return 0;
